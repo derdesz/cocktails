b63ebe1ba290be8d3e5fff4bfe2281a06947bc1b
derdesz <dora.mailbox@gmail.com>
2020-10-27 12:05:01 +0100

    adding Randomcocktail to App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import './App.css';
+import React from 'react';
+import BySpirit from './components/BySpirit';
+import RandomCocktail from './components/RandomCocktail';
 
 function App() {
   return (
     <div className="App">
+      <div className="spirit-list">
+        <BySpirit clickOnSpirit={clickOnSpirit}/>
+      </div>
+      <div className="three-random-cocktails">
+        <RandomCocktail/>
+        <RandomCocktail/>
+        <RandomCocktail/>
+      </div>
       
     </div>
   );
