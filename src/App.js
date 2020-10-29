b2d53a72e0ec4c622764e35491e566852847abaf
derdesz <dora.mailbox@gmail.com>
2020-10-29 11:10:01 +0100

    setting up route for individual cocktail (by id)

--- src/App.js
+++ src/App.js
@@ -6,26 +6,35 @@ import RandomCocktail from './components/RandomCocktail';
 import NavigationBar from "./components/NavigationBar.js";
 import FilterByAlcoholic from "./components/FilterByAlcoholic";
 import GetCocktailsBySpirit from "./components/GetCocktailsBySpirit";
+import GetCocktailById from './components/GetCocktailById';
 
 
 
 function App() {
   const [spiritName, setSpiritName] = useState("");
+  const [currentId, setCurrentId] = useState("");
 
   function clickOnSpirit (spiritName) {
+    console.log("Clicked");
     setSpiritName(spiritName);
   }
 
   const clickOnFilter = () => {
   }
 
+  const handleCardClick = (id) => {
+    setCurrentId(id);
+    console.log("clicked on card");
+  }
+
   return (
     <Router>
       <div className="App">
         <NavigationBar/>
+        <div>
+          <Route path="/by-spirit" render={(props) => (<BySpirit {...props} clickOnSpirit={clickOnSpirit}/>)}/>
+        </div>  
         <div className="spirit-list">
-          <h2>spirit list:</h2>
-          <BySpirit clickOnSpirit={clickOnSpirit}/>
           <div className="cocktail-container">
             <Route path="/alcoholic" render={(props) => (<FilterByAlcoholic {...props} clickOnFilter={clickOnFilter} category="Alcoholic" />)}/>
             <Route path="/non-alcoholic" render={(props) => (<FilterByAlcoholic {...props} clickOnFilter={clickOnFilter} category="Non_Alcoholic" />)}/>
@@ -37,10 +46,12 @@ function App() {
           <RandomCocktail />
           <RandomCocktail />
         </div>
-        <div className="cocktails-by-spirit">
-          <GetCocktailsBySpirit spiritName={spiritName} />
+        <div className="cocktail-container">
+          <Route path={"/by-spirit/" + spiritName} render={(props) => (<GetCocktailsBySpirit {...props} spiritName={spiritName} handleCardClick={handleCardClick}/>)}/>
         </div>
-
+        <div className="cocktail-container">
+          <Route path={"/" + currentId} render={(props) => (<GetCocktailById {...props} cocktailId={currentId} cocktailName={currentName} imgSrc={currentImgSrc} instructions={currentInstructions} category={currentCategory}/>)}/>
+        </div> 
       </div>
     </Router>
   );
