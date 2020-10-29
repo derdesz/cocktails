d8bc28831ab3ee3a4699ed8649ca0265f47bad82
g-georgia <gg.georgina20@gmail.com>
2020-10-29 22:02:26 +0100

    Format menu bar at by Spirit

--- src/components/BySpirit.js
+++ src/components/BySpirit.js
@@ -9,15 +9,19 @@ export default function(props){
       <div className="ui horizontal list">
         <div className="item">
           <Link className="content" to="/by-spirit/Vodka" onClick={() => {props.clickOnSpirit("Vodka")}}>Vodka Cocktails</Link>
+          <span className="line">|</span>
         </div>
         <div className="item">
           <Link className="content" to="/by-spirit/Gin" onClick={() => props.clickOnSpirit("Gin")}>Gin Cocktails</Link>
+          <span className="line">|</span>
         </div> 
         <div className="item">
           <Link className="content" to="/by-spirit/Rum" onClick={() => props.clickOnSpirit("Rum")}>Rum Cocktails</Link>
+          <span className="line">|</span>
         </div>
         <div className="item"> 
           <Link className="content" to="/by-spirit/Tequila" onClick={() => props.clickOnSpirit("Tequila")}>Tequila Cocktails</Link>
+          <span className="line">|</span>
         </div>
         <div className="item">
           <Link className="content" to="/by-spirit/Scotch" onClick={() => props.clickOnSpirit("Scotch")}>Scotch Cocktails</Link>
