1b761c14060e4276c169813c1340abfcd3ffdbc5
derdesz <dora.mailbox@gmail.com>
2020-11-12 15:44:06 +0100

    added picture to spirits and footer

--- src/components/RandomCocktailContainer.js
+++ src/components/RandomCocktailContainer.js
@@ -3,11 +3,22 @@ import RandomCocktail from './RandomCocktail';
 
 const RandomCocktailContainer = () => {
     return (
-        <div id="random-cocktail-container" className="ui grid container">
-            <RandomCocktail/>
-            <RandomCocktail/>
-            <RandomCocktail/>
+        <div className="ui three column grid">
+            <div className="column">
+                <RandomCocktail/>
+            </div>
+            <div className="column">
+                <RandomCocktail className="inner-div"/>
+            </div>
+            <div className="column">
+                <RandomCocktail/>
+            </div>
         </div>
+        // {/* <div id="random-cocktail-container" className="ui grid container">
+        //     <RandomCocktail/>
+        //     <RandomCocktail/>
+        //     <RandomCocktail/>
+        // </div> */}
     );
 }
 
