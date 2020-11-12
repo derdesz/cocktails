b51d055c2ea29dfa9f88ef86040f3b3514f1fa87
g-georgia <gg.georgina20@gmail.com>
2020-11-12 15:51:44 +0100

    Reorganize SpiritImage into rails

--- src/components/GetSpirit.js
+++ src/components/GetSpirit.js
@@ -24,14 +24,21 @@ const [spiritDescription, setSpiritDescription] = useState("");
     if (spiritDescriptionName !== "") {
         return (
             <React.Fragment>
-                <SpiritImage spirit={spiritDescriptionName + 1} number="1"/>
-
+                <div class="ui left internal rail">
+                    <div class="ui segment" id="rail-box">
+                        <SpiritImage spirit={spiritDescriptionName + 1} number="1"/>
+                    </div>
+                </div>
+                <div class="ui right internal rail">
+                    <div class="ui segment" id="rail-box">
+                        <SpiritImage spirit={spiritDescriptionName + 2} number="2"/>
+                    </div>
+                </div>
+            
                 <div className="ui raised segment" id="description-text">
                     <h4 className="ui header">{spiritDescriptionName}</h4>
                     <p id="about-text">{spiritDescription}</p>
                 </div>
-
-                <SpiritImage spirit={spiritDescriptionName + 2} number="2"/>
             </React.Fragment>
         );
     } else return <div></div>;
