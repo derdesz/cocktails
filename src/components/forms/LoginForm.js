74454bc09850fbe94d6faa379588035ea361ad41
derdesz <dora.mailbox@gmail.com>
2020-12-08 11:52:07 +0100

    add post request to "/login" endpoint with user login data

--- src/components/forms/LoginForm.js
+++ src/components/forms/LoginForm.js
@@ -1,6 +1,27 @@
-import React from 'react'
+import React, {useState} from 'react'
+import * as axios from "axios";
 
 export default function LoginForm() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleEmailChange = (event) => {
+        setEmail(event.target.value);
+    }
+
+    const handlePasswordChange = (event) => {
+        setPassword(event.target.value);
+    }
+
+    const clickOnLogIn = () => {
+        const user = {
+            email: email,
+            password: password
+        };
+
+        axios.post("http://localhost:8080/login", user);
+    }
+
     return (
         <React.Fragment>
         <h2>Log in</h2>
@@ -9,13 +30,13 @@ export default function LoginForm() {
                 <div className="ui inverted form right-align-form">
                     <div className="inline field">
                         <label>E-mail</label>
-                        <input type="text" placeholder="E-mail"/>
+                        <input type="text" placeholder="E-mail" value={email} onChange={handleEmailChange}/>
                     </div>
                     <div className="inline field">
                         <label>Password</label>
-                        <input type="password" placeholder="Password"/>
+                        <input type="password" placeholder="Password" value={password} onChange={handlePasswordChange}/>
                     </div>
-                    <div className="ui submit button">Log in</div>
+                    <div className="ui submit button" onClick={clickOnLogIn}>Log in</div>
                 </div>
             </div>
         </div>
