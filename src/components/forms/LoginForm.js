c874e63d954a23416147673271860923d5d62f1b
g-georgia <gg.georgina20@gmail.com>
2020-12-10 09:58:14 +0100

    Change login post request to store user credencials

--- src/components/forms/LoginForm.js
+++ src/components/forms/LoginForm.js
@@ -4,9 +4,11 @@ import * as axios from "axios";
 export default function LoginForm() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [username, setUsername] = useState("");
 
     const handleEmailChange = (event) => {
         setEmail(event.target.value);
+        setUsername(event.target.value);
     }
 
     const handlePasswordChange = (event) => {
@@ -14,12 +16,24 @@ export default function LoginForm() {
     }
 
     const clickOnLogIn = () => {
-        const user = {
-            email: email,
-            password: password
-        };
+        var bodyFormData = new FormData();
+        bodyFormData.append('username', username);
+        bodyFormData.append('password', password);
 
-        axios.post("http://localhost:8080/login", user);
+        axios({
+            method: 'post',
+            url: 'http://localhost:8080/login',
+            data: bodyFormData,
+            headers: {'Content-Type': 'multipart/form-data' },
+            withCredentials: true,
+            })
+            .then(res => {
+                console.log("yeh we have succeeded");
+                window.location.href = 'http://localhost:3000';
+              })
+              .catch(er => {
+                console.log("no data sorry ", er);
+              });
     }
 
     return (
@@ -30,11 +44,11 @@ export default function LoginForm() {
                 <div className="ui inverted form right-align-form">
                     <div className="inline field">
                         <label>E-mail</label>
-                        <input type="text" placeholder="E-mail" value={email} onChange={handleEmailChange}/>
+                        <input type="text" placeholder="E-mail" value={email} name="username" onChange={handleEmailChange}/>
                     </div>
                     <div className="inline field">
                         <label>Password</label>
-                        <input type="password" placeholder="Password" value={password} onChange={handlePasswordChange}/>
+                        <input type="password" placeholder="Password" value={password} name="password" onChange={handlePasswordChange}/>
                     </div>
                     <div className="ui submit button" onClick={clickOnLogIn}>Log in</div>
                 </div>
