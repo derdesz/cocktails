9db28ca67d22966f577a5b449cfc9c48baa4d824
derdesz <dora.mailbox@gmail.com>
2020-12-10 11:49:47 +0100

    handle "logged in as user"

--- src/components/forms/LoginForm.js
+++ src/components/forms/LoginForm.js
@@ -1,61 +1,84 @@
-import React, {useState} from 'react'
-import * as axios from "axios";
+import React, { useState } from "react";
+import axios from "axios";
+import {Redirect} from "react-router";
 
-export default function LoginForm( {logIn} ) {
+
+
+export default function LoginForm({logIn}) {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-
+    const [username, setUsername] = useState("");
+    const [successfulLogin, setSuccessFulLogin] = useState(false);
 
     const handleEmailChange = (event) => {
         setEmail(event.target.value);
+        setUsername(event.target.value);
     }
 
-    const handlePasswordChange = (event) => {
-        setPassword(event.target.value);
-    }
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
 
-    const checkValidLogin = (response) => {
-        if (response.data.name) {
-            logIn(response.data.name);
-            console.log(response.data.name);
-        } else {
-            alert("not ok");
-        }
-
+  const checkValidLogin = (name) => {
+    if (name) {
+        logIn(name);
+    } else {
+      alert("not ok");
     }
+  };
 
     const clickOnLogIn = () => {
+        var bodyFormData = new FormData();
+        bodyFormData.append('username', username);
+        bodyFormData.append('password', password);
+
         const user = {
             email: email,
             password: password
         };
+        axios({
+            method: 'post',
+            url: 'http://localhost:8080/login',
+            data: bodyFormData,
+            user: user,
+            headers: {'Content-Type': 'multipart/form-data' },
+            withCredentials: true,
+            })
+            .then(res => {
+                checkValidLogin(res.config.user.email);
+                setSuccessFulLogin(true);
 
-        axios.post("http://localhost:8080/login", user).then(response => {
-            checkValidLogin(response);
-        });
-        setEmail("");
-        setPassword("");
+                // window.location.href = 'http://localhost:3000';
+
+              })
+              .catch(er => {
+                console.log("no data sorry ", er);
+              });
     }
 
-    return (
-        <React.Fragment>
-            <h2>Log in</h2>
-            <div className="ui inverted segment">
-                <div className="centered-form">
-                    <div className="ui inverted form right-align-form">
-                        <div className="inline field">
-                            <label>E-mail</label>
-                            <input type="text" placeholder="E-mail" value={email} onChange={handleEmailChange}/>
+    if(successfulLogin) {
+        return <Redirect to="/"/>
+    } else {
+        return (
+            <React.Fragment>
+                <h2>Log in</h2>
+                <div className="ui inverted segment">
+                    <div className="centered-form">
+                        <div className="ui inverted form right-align-form">
+                            <div className="inline field">
+                                <label>E-mail</label>
+                                <input type="text" placeholder="E-mail" value={email} name="username" onChange={handleEmailChange}/>
+                            </div>
+                            <div className="inline field">
+                                <label>Password</label>
+                                <input type="password" placeholder="Password" value={password} name="password" onChange={handlePasswordChange}/>
+                            </div>
+                            <div className="ui submit button" onClick={clickOnLogIn}>Log in</div>
                         </div>
-                        <div className="inline field">
-                            <label>Password</label>
-                            <input type="password" placeholder="Password" value={password}
-                                   onChange={handlePasswordChange}/>
-                        </div>
-                        <div className="ui submit button" onClick={clickOnLogIn}>Log in</div>
                     </div>
                 </div>
-            </div>
-        </React.Fragment>
-    )
+            </React.Fragment>
+        )
+    }
+
 }
