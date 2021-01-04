fd0078545baad3a1e3babfa078f6b31cc7baf454
Kattkovacs <kattkovacs@gmail.com>
2021-01-04 11:14:09 +0100

    Add logout.js

--- src/components/LoginButton.js
+++ src/components/LoginButton.js
@@ -1,13 +1,21 @@
 import React from "react";
 import Nav from "react-bootstrap/Nav";
-import {useCookies} from "react-cookie";
+import { useCookies } from "react-cookie";
 
-export default function LoginButton () {
-    const [cookies, setCookie] = useCookies(["email"]);
+export default function LoginButton() {
+  const [cookies, setCookie] = useCookies(["email"]);
 
-    if(cookies.email.length > 0) {
-        return null;
-    } else {
-        return <Nav.Link className="ui inverted red basic button" id="login-link" href="/login">Log in</Nav.Link>
-    }
+  if (cookies.email) {
+    return null;
+  } else {
+    return (
+      <Nav.Link
+        className="ui inverted red basic button"
+        id="login-link"
+        href="/login"
+      >
+        Log in
+      </Nav.Link>
+    );
+  }
 }
